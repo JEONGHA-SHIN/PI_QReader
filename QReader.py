0dab9199e59f28d4c84b34ff1a8285e57423be79
filab <factoryintelligence@naver.com>
2020-08-26 15:53:03 +0100

    add tcp send

--- QReader.py
+++ QReader.py
@@ -1,38 +1,80 @@
 import pyzbar.pyzbar as pyzbar
+from threading import *
 import cv2
+import server
+import image_server
+import time
+import socket
 
-cap = cv2.VideoCapture(0)
+class QReader:
+    def __init__(self):
+        self.s = server.ServerSocket()
+        self.i_s = image_server.ImageServer()
 
-i = 0
-while(cap.isOpened()):
-  ret, img = cap.read()
+        
+        ip = socket.gethostbyname("raspberrypi.local")
+        print(ip)
 
-  if not ret:
-    continue
+        self.s.start(ip, 9292)
+        self.i_s.start(ip,9293)
 
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def detectQR(self):
+        text = "waiting...\n"
+        pre_text = ""
+        pre_time = 0
+        self.cap = cv2.VideoCapture(0)
+        while(self.cap.isOpened()):
+            ret, img = self.cap.read()
+
+            if not ret:
+                continue
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
      
-  decoded = pyzbar.decode(gray)
+            decoded = pyzbar.decode(gray)
+            if decoded != []:
+                for d in decoded: 
+                    x, y, w, h = d.rect
 
-  for d in decoded: 
-    x, y, w, h = d.rect
+                    barcode_data = d.data.decode("utf-8")
+                    barcode_type = d.type
+                    #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-    barcode_data = d.data.decode("utf-8")
-    barcode_type = d.type
+                    text = str(barcode_data)
+                    print(text)
 
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                if pre_text != text:
+                    self.s.send(text)
+                    self.i_s.send(img)
+                    print("sending...",text)
+                    pre_text = text
+                pre_time = time.time()
 
-    text = '%s (%s)' % (barcode_data, barcode_type)
-    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
-    print(text)
-  cv2.imshow('img', img)
+            else:
+                if time.time() - pre_time > 3:
+                    text = "waiting...\n"
+                    if pre_text != text:
+                        self.s.send(text)
+                        self.i_s.send(img)
+                        print("sending...",text)
+                        pre_text = text
+                    pre_time = time.time()
+                print(time.time() - pre_time)
 
-  key = cv2.waitKey(1)
-  if key == ord('q'):
-    break
-  elif key == ord('s'):
-    i += 1
-    cv2.imwrite('c_%03d.jpg' % i, img)
+                #cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
+            #cv2.imshow('img', img)
 
-cap.release()
-cv2.destroyAllWindows()
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+            #time.sleep(0.5)
+
+    def __del__(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+        self.s.stop()
+        self.i_s.stop()
+
+if __name__ == "__main__":
+    Q = QReader()
+    Thread(target=Q.detectQR(), args=())
+
---
+++ img-server.py
@@ -0,0 +1,123 @@
+from threading import *
+from socket import *
+
+ 
+class ImageServer:
+ 
+    def __init__(self):        
+
+        self.bListen = False       
+        self.clients = []
+        self.ip = []
+        self.threads = []
+         
+    def __del__(self):
+        self.stop()
+ 
+    def start(self, ip, port):
+        self.server = socket(AF_INET, SOCK_STREAM)            
+ 
+        try:
+            self.server.bind( (ip, port))
+        except Exception as e:
+            print('Bind Error : ', e)
+            return False
+        else:                 
+            self.bListen = True
+            self.t = Thread(target=self.listen, args=(self.server,))
+            self.t.start()
+            print('Server Listening...')
+ 
+        return True
+ 
+    def stop(self):
+        self.bListen = False
+        if hasattr(self, 'server'):            
+            self.server.close()            
+            print('Server Stop')
+ 
+    def listen(self, server):
+        while self.bListen:
+            server.listen(5)   
+            try:
+                client, addr = server.accept()
+            except Exception as e:
+                print('Accept() Error : ', e)
+                break
+            else:                
+                self.clients.append(client)
+                self.ip.append(addr)                
+                self.conn.conn_signal.emit()                
+                t = Thread(target=self.receive, args=(addr, client))
+                self.threads.append(t)
+                t.start()                
+                 
+        self.removeAllClients()
+        self.server.close()
+ 
+    def receive(self, addr, client):
+        while True:            
+            try:
+                recv = client.recv(1024)                
+            except Exception as e:
+                print('Recv() Error :', e)                
+                break
+            else:                
+                msg = str(recv, encoding='utf-8')
+                if msg:
+                    self.send(msg)
+                    print('[RECV]:', addr, msg)
+                    return msg
+ 
+         
+        self.removeCleint(addr, client)
+ 
+    def send(self, frame):
+        try:
+            for c in self.clients:
+		msg = img2text(frame)
+                c.send(msg.encode())
+        except Exception as e:
+            print('Send() Error : ', e)
+
+
+    def img2text(frame):
+        encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+        result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+
+        data = numpy.array(imgencode)
+        stringData = data.tostring()
+
+        return stringData
+ 
+    def removeCleint(self, addr, client):
+        client.close()
+        self.ip.remove(addr)
+        self.clients.remove(client)
+ 
+        self.conn.conn_signal.emit()
+         
+        i=0
+        for t in self.threads[:]:
+            if not t.isAlive():
+                del(self.threads[i])
+            i+=1
+ 
+        self.resourceInfo()
+ 
+    def removeAllClients(self):
+        for c in self.clients:
+            c.close()
+ 
+        self.ip.clear()
+        self.clients.clear()
+        self.threads.clear()
+ 
+        self.conn.conn_signal.emit()
+ 
+        self.resourceInfo()
+ 
+    def resourceInfo(self):
+        print('Number of Client ip\t: ', len(self.ip) )
+        print('Number of Client socket\t: ', len(self.clients) )
+        print('Number of Client thread\t: ', len(self.threads) )
